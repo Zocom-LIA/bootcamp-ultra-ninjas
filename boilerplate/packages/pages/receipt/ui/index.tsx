227587919f93f35d43e0ece3e887d1e2158361f9
tweppy <tweppy@gmail.com>
2024-01-03 13:36:14 +0100

    receipt page and component

--- boilerplate/packages/pages/receipt/ui/index.tsx
+++ boilerplate/packages/pages/receipt/ui/index.tsx
@@ -1,12 +1,22 @@
-import { Header } from '@zocom/header';
-import './style.scss';
-import React from 'react'
+import { Header } from "@zocom/header";
+import "./style.scss";
+import { Button, ButtonType } from "@zocom/button";
+import { StyleTypes } from "@zocom/types";
+import { ReceiptComponent } from "@zocom/receiptcomponent";
+import { Link } from "react-router-dom";
 
 export const Receipt = () => {
   return (
-    <div>
-    <Header />
-    <p>Receipt</p>
-  </div>
-  )
-}
+    <div className="receipt">
+      <Header />
+      <section className="receipt__section">
+        <ReceiptComponent />
+        <Link to="/menu">
+          <Button type={ButtonType.STRETCH} style={StyleTypes.DARK}>
+            Gör en ny beställning
+          </Button>
+        </Link>
+      </section>
+    </div>
+  );
+};
