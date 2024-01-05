792e822d9ed9354bfdf202e1e9192e9e5fee68ab
Pile-o-pancakes <112945412+Pile-o-pancakes@users.noreply.github.com>
2024-01-05 13:40:13 +0100

    edits

---
+++ backend/packages/interfaces/interfaces/index.ts
@@ -0,0 +1,11 @@
+export interface MenuItem {
+    name: string,
+    desc: string,
+    ingredients: string[],
+    price: number
+}
+
+export interface User {
+    name: string,
+    password: string
+}
--- backend/packages/interfaces/interfaces/menuItem.ts
+++
@@ -1,8 +0,0 @@
-interface MenuItem {
-    name: string,
-    desc: string,
-    ingredients: string[],
-    price: number
-}
-
-export default MenuItem;
