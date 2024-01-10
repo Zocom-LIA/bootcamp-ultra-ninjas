33dc83bf3734f3a7070acf5555372a0e4bec2d23
markopaunovic1 <marpau970514@hotmail.com>
2024-01-10 15:05:37 +0100

    fixing

--- backend/src/functions/getOrder/index.ts
+++ backend/src/functions/getOrder/index.ts
@@ -2,15 +2,30 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { db } from "../../services/db";
 import middy from "@middy/core";
 import { sendResponse, sendError } from "../../responses";
+import * as menuData from "../../../../data/menu.json";
+import { CartItem, Order, MenuItem } from "../../interfaces";
 
 const getOrder = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     // Add code here
+    const OrderTable = {
+      TableName: 'YYGSOrders'
+    }
+
+    const scannedTable = await db.scan(OrderTable).promise();
+
+    if (scannedTable.Items && Array.isArray(scannedTable.Items)) {
+      const usersOrder = scannedTable.Items.filter(order => order.id === order.id)
+    
     return sendResponse(200, {
       success: true,
-      message: "message",
+      message: "successfully fetched users order",
+      body: usersOrder
     });
-  } catch (error) {
+    } else {
+      return sendError(404, { success: false, message: "Items not found or invalid format" });
+    }
+  } catch  (error) {
     return sendError(500, { success: false, message: "Internal Server Error" });
   }
 };
