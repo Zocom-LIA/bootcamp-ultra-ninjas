f20f8825794604bb8bd9524ae1553cdd2d198da8
markopaunovic1 <marpau970514@hotmail.com>
2024-01-10 13:27:09 +0100

    setting up placeorder

--- backend/src/functions/placeOrder/index.ts
+++ backend/src/functions/placeOrder/index.ts
@@ -2,13 +2,30 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { db } from "../../services/db";
 import middy from "@middy/core";
 import { sendResponse, sendError } from "../../responses";
+import { CartItem, Order, MenuItem } from "../../interfaces";
 
 const placeOrder = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
     // Add code here
+
+    const requestBody = JSON.parse(event.body || '');
+    const { cartItems } = requestBody;
+
+    // Create an order object
+    const order: Order = {
+      id: generateOrderId(),
+      items: cartItems,
+    }
+
+    await db.put({
+      TableName: 'YYGSOrders',
+      Item: order
+    })
+
     return sendResponse(200, {
       success: true,
-      message: "message",
+      message: "Order placed successfully",
+      body: {order},
     });
   } catch (error) {
     return sendError(500, { success: false, message: "Internal Server Error" });
@@ -16,3 +33,7 @@ const placeOrder = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyR
 };
 
 export const handler = middy(placeOrder).handler(placeOrder);
+
+function generateOrderId(): string {
+  return '#' +  Math.random().toString(36).substring(2,12);
+}
