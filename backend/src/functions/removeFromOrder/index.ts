d4fdb91f18ec584fae0bcf20a10ddd4566e3674b
Pile-o-pancakes <112945412+Pile-o-pancakes@users.noreply.github.com>
2024-01-11 10:38:51 +0100

    klar

--- backend/src/functions/removeFromOrder/index.ts
+++ backend/src/functions/removeFromOrder/index.ts
@@ -5,13 +5,38 @@ import { sendResponse, sendError } from "../../responses";
 
 const removeFromOrder = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   try {
-    // Add code here
-    return sendResponse(200, {
-      success: true,
-      message: "message",
-    });
-  } catch (error) {
-    return sendError(500, { success: false, message: "Internal Server Error" });
+
+      const orderId:string = event.pathParameters!.orderId!.toString();
+      
+
+      const result = await db.delete ({
+        TableName: "YYGSOrders",
+        Key: {
+          id: orderId
+        },
+        ReturnValues: "ALL_OLD"
+      }).promise();
+
+      console.log(result.Attributes)
+
+      if(result.Attributes?.id != undefined) {
+
+        return sendResponse(200, {
+          success: true,
+          message: `Order ${result.Attributes!.id} avslutad`
+        });
+      }
+      else {
+
+        return sendResponse(404, {
+          success: true,
+          message: `Ingen order med id ${orderId} hittades`
+        })
+      }
+  }
+  catch (error) {
+
+    return sendError(500, { success: false, message: "Error" });
   }
 };
 
